c9fcbf5be0845024862bfdd7981872488be492f7
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-09-30 08:25:00 +0000

    Terminado

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 #Ejercicio 1
-num1 = input("Escribe un numero: ")
-print(type(num1))
 
-num2 = int(input("Escribe un numero entero: "))
-print(type(num2))
+num1 = int(input("Escribe un numero entero: "))
+print(f"El numero {num1} es un numero {type(num1)}")
 
-num3 = float(input("Escribe un numero racional: "))
-print(type(num3))
+num2 = float(input("Escribe un numero racional: "))
+print(f"El numero {num2} es un numero {type(num2)}")
 
 #Ejercicio 2
 numero1 = int(input("Dime un numero entero: "))
-print(str(numero1).zfill(4+len(str(numero1))))
+print(str(numero1).zfill(5))
 numero2 = float(input("Dime un numero racional: "))
-print(str(numero1).zfill(4+len(str(numero1)))) + print("{:.03f}".format(numero2))
+print("{:09.03f}".format(numero2)) 
 
 #Ejercicio 3 
 altura = float(input("Dime tu estatura en metros: "))
 peso = float(input("Dime tu peso en kilos: "))
-print(f"Mides {altura} metros y pesas {peso}")
+print(f"Mides {altura} metros y pesas {peso}kg")
 print("Mides {0} metros y pesas {1} Kg".format(altura, peso))
 print("Mides {0:<5} metros y pesas {1:>5}Kg".format(altura, peso))
 
